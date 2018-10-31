70e318f3c8f405d05b0bd67dfae0ec16a4680f9b
EC2 Default User <ec2-user@ip-172-31-4-244.us-east-2.compute.internal>
2018-10-31 09:31:04 +0000

    MODIFY templete to DRY

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,13 +6,34 @@ class MoviesController < ApplicationController
         id = params[:id]
         @movie = Movie.find(id)
     end
+#     def create
+#         @movie = Movie.new(params[:movie])
+#         if @movie.save
+#             flash[:notice] = "#{@movie.title} was successfully created."
+#             redirect_to movies_path
+#         else
+#             render 'new' # note, 'new' template can access @movie's field values!
+#         end
+#     end
+#     # replaces the 'update' method in controller:
+#     def update
+#         @movie = Movie.find params[:id]
+#         if @movie.update_attributes(params[:movie])
+#             flash[:notice] = "#{@movie.title} was successfully updated."
+#             redirect_to movie_path(@movie)
+#         else
+#             render 'edit' # note, 'edit' template can access @movie's field values!
+#         end
+#     end
+# # note, you will also have to update the 'new' method:
+#     def new
+#         @movie = Movie.new
+#     end
     def new
         @movie = Movie.new
         #default render :'new' template
     end
     def create
-        #params.require(:movie)
-        #params[:movie].permit(:title, :rating, :release_date)
         @movie = Movie.create!(allowparams)
         flash[:warning] = "#{@movie.title} was successfully created"
         redirect_to movie_path (@movie)
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,26 @@
 class Movie < ActiveRecord::Base
+#   def self.all_ratings ; %w[G PG PG-13 R NC-17] ; end #  shortcut: array of strings
+#   validates :title, :presence => true
+#   validates :release_date, :presence => true
+#   validate :released_1930_or_later # uses custom validator below
+#   validates :rating, :inclusion => {:in => Movie.all_ratings},
+#     :unless => :grandfathered?
+#   def released_1930_or_later
+#     errors.add(:release_date, 'must be 1930 or later') if
+#     release_date && release_date < Date.parse('1 Jan 1930')
+#   end
+#   @@grandfathered_date = Date.parse('1 Nov 1968')
+#   def grandfathered?
+#     release_date && release_date < @@grandfathered_date
+#   end
+# end
+# # try in console:
+# m = Movie.new(:title => '', :rating => 'RG', :release_date => '1929-01-01')
+# # force validation checks to be performed:
+# m.valid?  # => false
+# m.errors[:title] # => ["can't be blank"]
+# m.errors[:rating] # => [] - validation skipped for grandfathered movies
+# m.errors[:release_date] # => ["must be 1930 or later"]
+# m.errors.full_messages # => ["Title can't be blank", "Rating is not
+#     included in the list", "Release date must be 1930 or later"]
 end
